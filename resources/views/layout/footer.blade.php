de30234656b6f6ecf13f8453dbe7573acb13d962
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2023-05-21 13:12:14 +0200

    Update working in Linux

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <?php echo date("Y"); ?> <strong><span>KelvinCodes</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer>
+  <!-- End  Footer -->
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,31 @@
+<!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container-fluid d-flex justify-content-between align-items-right">
+
+      <h1 class="logo me-auto me-lg-0"><a href="/">KelvinCodes</a></h1>
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="active" href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/resume">Resume</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="https://status.kelvincodes.nl/status/main" target="_blank">Network status</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links">
+        <a href="ttps://github.com/kelvin-codes-stuff" class="github"><i class="bi bi-github"></i></a>
+        <a href="https://kelvincodes.nl/discord_invite" class="discord"><i class="bi bi-discord"></i></a>
+        <a href="https://www.facebook.com/kelvhc4l/" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/kelvincodes.nl/" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/kelvin-de-reus-052141152" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+
+  </header>
+<!-- End Header -->
+
